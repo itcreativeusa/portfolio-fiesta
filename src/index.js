d6d3902f413c0375d2eb375c9b480b7da499dbcd
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-12 19:38:04 -0700

    add more comments

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// render the app component to the root element
+// Render the app component to the root element
 ReactDOM.render(<App />, document.getElementById("root"));
