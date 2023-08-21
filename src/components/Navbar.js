864e6c585328308b08d7a2921b572f9051a2780b
Kateryna Stetsenko <itcreativeusa@gmail.com>
2023-08-20 19:14:46 -0700

    add mobile styles for smaller screens to make webpage responcive for navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,50 +14,52 @@ const styles = {
     lineHeight: "1.5",
     height: "300px",
     fontSize: "2rem",
+    color: "magenta",
     fontWeight: "600",
   },
 };
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/"></Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link className="nav-link" to="/">
-              About Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/project-all">
-              All projects
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/contact">
-              Contact Me
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/resume">
-              My resume
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div style={styles.heading}>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/project-all">
+                All projects
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact">
+                Contact Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/resume">
+                My resume
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 
